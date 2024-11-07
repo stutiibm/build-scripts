0aa3f4c9537f504d73a670da5b64e131026fcbe7
stutiibm <Stuti.Wali@ibm.com>
2024-11-07 14:04:50 +0000

    sbm: Added ubi9 script for shelljs

--- s/shelljs/shelljs_ubi_9.3.sh
+++ s/shelljs/shelljs_ubi_9.3.sh
@@ -26,7 +26,7 @@ PACKAGE_NAME=shelljs
 PACKAGE_VERSION=${1:-master}
 PACKAGE_URL=https://github.com/shelljs/shelljs
 export NODE_VERSION=${NODE_VERSION:-20}
-
+HOME_DIR=${PWD}
 
 sudo yum install -y yum-utils git wget tar gzip python3 python3-devel gcc gcc-c++ make cmake
 
