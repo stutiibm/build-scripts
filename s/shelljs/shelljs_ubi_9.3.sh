4edaa829d2f87e69b9c491176176b14e1e873d18
stutiibm <Stuti.Wali@ibm.com>
2024-11-08 04:55:31 +0000

    Sbm:updated ubi 9 script

--- s/shelljs/shelljs_ubi_9.3.sh
+++ s/shelljs/shelljs_ubi_9.3.sh
@@ -28,12 +28,10 @@ HOME_DIR=${PWD}
 sudo yum install -y yum-utils git wget tar gzip python3 python3-devel gcc gcc-c++ make cmake libcurl-devel
 
 #Installing Nodejs 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source "$HOME"/.bashrc
-echo "installing nodejs $NODE_VERSION"
-nvm install "$NODE_VERSION" >/dev/null
-nvm use $NODE_VERSION
-
+wget https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-ppc64le.tar.gz
+tar -xzf node-v${NODE_VERSION}-linux-ppc64le.tar.gz
+export PATH=$HOME_DIR/node-v${NODE_VERSION}-linux-ppc64le/bin:$PATH
+node -v
 
 #Cloning repo
 git clone $PACKAGE_URL
