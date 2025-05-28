46f49955a6336a9bc596ba64ab80c93e7fa80bb4
stutiibm <110608613+stutiibm@users.noreply.github.com>
2025-05-28 10:50:35 +0530

    Update trivy_code_scan.sh

--- script/scanner-scripts/trivy_code_scan.sh
+++ script/scanner-scripts/trivy_code_scan.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -e
 
+echo "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$-inside trivy_code_scan.sh$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
 validate_build_script=$VALIDATE_BUILD_SCRIPT
 cloned_package=$CLONED_PACKAGE
 cd package-cache
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+echo $validate_build_script
 
 if [ $validate_build_script == true ];then
+        echo "+++++++++++++++++++++++++++inside if++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 	TRIVY_VERSION=$(curl -s https://api.github.com/repos/aquasecurity/trivy/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
 	wget https://github.com/aquasecurity/trivy/releases/download/${TRIVY_VERSION}/trivy_${TRIVY_VERSION#v}_Linux-PPC64LE.tar.gz
 	tar -xzf trivy_${TRIVY_VERSION#v}_Linux-PPC64LE.tar.gz
@@ -16,3 +20,5 @@ if [ $validate_build_script == true ];then
 	sudo trivy -q fs --timeout 30m -f cyclonedx ${cloned_package} > trivy_source_sbom_results.cyclonedx
  	#cat trivy_source_sbom_results.cyclonedx
  fi
+
+ echo "+++++++++++++++++++++++++++outside if+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
