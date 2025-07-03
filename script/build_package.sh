940aeb480bbcc95a1871d382dc11cbe7f7f2f89d
stutiibm <110608613+stutiibm@users.noreply.github.com>
2025-07-03 16:01:43 +0530

    Update build_package.sh to pythonbuffer

--- script/build_package.sh
+++ script/build_package.sh
@@ -37,8 +37,8 @@ else
     fi  
 fi
 
-python3 script/validate_builds_currency.py "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "$docker_image" > build_log 2>&1 &
-
+#python3 script/validate_builds_currency.py "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "$docker_image" > build_log 2>&1 &
+PYTHONUNBUFFERED=1 python3 script/validate_builds_currency.py "$PKG_DIR_PATH$BUILD_SCRIPT" "$VERSION" "$docker_image" > build_log 2>&1 &
 SCRIPT_PID=$!
 # Start tailing the log in the background
 tail -f build_log &
