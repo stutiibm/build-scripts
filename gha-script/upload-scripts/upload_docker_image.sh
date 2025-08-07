d5670afed9eaa25a7521bedbb9094292ade66e45
stutiibm <110608613+stutiibm@users.noreply.github.com>
2025-08-07 14:23:43 +0530

    Update upload_docker_image.sh

--- gha-script/upload-scripts/upload_docker_image.sh
+++ gha-script/upload-scripts/upload_docker_image.sh
@@ -11,31 +11,6 @@ echo "----------------Lowering package name--------------------------"
 package_name=$(echo $PACKAGE_NAME | tr '[:upper:]' '[:lower:]')
 echo "-----------------Tagging image---------------------------"
 
-echo "----------------Validating required environment variables---------------------------"
-
-missing_vars=0
-
-if [[ -z "$PACKAGE_NAME" ]]; then
-    echo "❌ PACKAGE_NAME is not set"
-    missing_vars=1
-fi
-
-if [[ -z "$IMAGE_NAME" ]]; then
-    echo "❌ IMAGE_NAME is not set"
-    missing_vars=1
-fi
-
-if [[ -z "$VERSION" ]]; then
-    echo "❌ VERSION is not set"
-    missing_vars=1
-fi
-
-if [[ $missing_vars -eq 1 ]]; then
-    echo "❌ One or more required environment variables are missing. Exiting script."
-    exit 1
-fi
-
-
 docker tag $IMAGE_NAME icr.io/ose4power-packages/$package_name-ppc64le:$VERSION
 echo "-----------------Pushing image---------------------------"
 docker push "icr.io/ose4power-packages/$package_name-ppc64le:$VERSION"
