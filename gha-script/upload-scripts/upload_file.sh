3f400ae969f63a80d57d229aee3733fae7ed9d06
stutiibm <110608613+stutiibm@users.noreply.github.com>
2025-08-18 20:14:33 +0530

    Update upload_file.sh to save file in correct path

--- gha-script/upload-scripts/upload_file.sh
+++ gha-script/upload-scripts/upload_file.sh
@@ -19,7 +19,7 @@ if [[ $(echo "$token_request" | jq -r '.errorCode') == "null" ]]; then
     token=$(echo "$token_request" | jq -r '.access_token')
     
     # curl command for uploading the file
-    response=$(curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.us.cloud-object-storage.appdomain.cloud/ose-power-toolci-bucket-stag//$1")
+    response=$(curl -X PUT -H "Authorization: bearer $token" -H "Content-Type: application/gzip" -T $1 "https://s3.us.cloud-object-storage.appdomain.cloud/ose-power-toolci-bucket-stag/$PACKAGE_NAME/$VERSION/$1")
                                                                                                         
     # Check if the PUT request was successful based on the absence of an <Error> block
     if ! echo "$response" | grep -q "<Error>"; then
