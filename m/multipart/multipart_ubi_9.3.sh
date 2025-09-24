17f1e69c8cdcd60952508ec1caf489b2e51e38e9
stutiibm <Stuti.Wali@ibm.com>
2025-09-24 14:21:27 +0000

    Added export comment

--- m/multipart/multipart_ubi_9.3.sh
+++ m/multipart/multipart_ubi_9.3.sh
@@ -18,6 +18,7 @@
 #
 # ----------------------------------------------------------------------------
 
+#export 
 PACKAGE_NAME=multipart
 PACKAGE_VERSION=${1:-v1.2.1}
 PACKAGE_URL=https://github.com/defnull/multipart
