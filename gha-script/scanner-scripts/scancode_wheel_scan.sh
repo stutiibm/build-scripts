812c6d33e7436cf45d70d043e014ac70d8c88dd2
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2025-10-15 18:36:32 +0530

    Update scancode_wheel_scan.sh

--- gha-script/scanner-scripts/scancode_wheel_scan.sh
+++ gha-script/scanner-scripts/scancode_wheel_scan.sh
@@ -1,23 +1,25 @@
 #!/bin/bash -e
 
-validate_build_script=$VALIDATE_BUILD_SCRIPT
-cloned_package=$CLONED_PACKAGE
+# validate_build_script=$VALIDATE_BUILD_SCRIPT
+# cloned_package=$CLONED_PACKAGE
 
-cd package-cache
+#cd package-cache
 
-if [ "$validate_build_script" == true ]; then  
+#if [ "$validate_build_script" == true ]; then  
   
-  echo "Installing ScanCode Toolkit..."
+  echo "------------------Installing ScanCode Toolkit..."
   SCANCODE_VERSION=v32.4.0
+  echo "===============wgetting=================="
   wget https://github.com/nexB/scancode-toolkit/archive/refs/tags/${SCANCODE_VERSION}.tar.gz
+  echo "===============tarring=================="
   tar -xzf ${SCANCODE_VERSION}.tar.gz
   SCANCODE_DIR="scancode-toolkit-${SCANCODE_VERSION#v}"
   cd $SCANCODE_DIR
   echo "===============Python version=================="
   python --version
   echo "===============Python version=================="
-  python -m pip install -e .
-  export PYTHONWARNINGS="ignore"
-  export PATH=$HOME/scancode-toolkit/venv/bin:$PATH
-  scancode --version
-fi
+  # python -m pip install -e .
+  # export PYTHONWARNINGS="ignore"
+  # export PATH=$HOME/scancode-toolkit/venv/bin:$PATH
+  # scancode --version
+#fi
