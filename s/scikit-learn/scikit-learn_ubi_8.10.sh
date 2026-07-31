a645710c70bfa5f9afff7dc2e50a3236504aff9a
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2026-08-01 00:13:06 +0530

    Update scikit-learn_ubi_8.10.sh

--- s/scikit-learn/scikit-learn_ubi_8.10.sh
+++ s/scikit-learn/scikit-learn_ubi_8.10.sh
@@ -23,7 +23,7 @@ PACKAGE_VERSION=${1:-1.5.0}
 PACKAGE_URL=https://github.com/scikit-learn/scikit-learn.git
 
 yum install -y python3.11 python3.11-pip python3.11-devel gcc gcc-c++ gcc-gfortran gcc-toolset-13 git
-yum install -y openblas-devel --enablerepo=codeready-builder-for-ubi-8-ppc64le-rpms
+yum install -y openblas-devel
 #yum install -y openblas-devel --enablerepo=codeready-builder-for-rhel-8-ppc64le-rpms
 
 source /opt/rh/gcc-toolset-13/enable
