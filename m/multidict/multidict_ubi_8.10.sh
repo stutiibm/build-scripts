cf216ba1306dd8c4533805e04076fb0bf61419a4
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2026-08-03 18:04:28 +0530

    Update multidict_ubi_8.10.sh

--- m/multidict/multidict_ubi_8.10.sh
+++ m/multidict/multidict_ubi_8.10.sh
@@ -2,7 +2,7 @@
 # -----------------------------------------------------------------------------
 #
 # Package          : multidict
-# Version          : 6.4.0
+# Version          : v6.4.0
 # Source repo      : https://github.com/aio-libs/multidict.git
 # Tested on        : UBI:8.10
 # Language         : Python
