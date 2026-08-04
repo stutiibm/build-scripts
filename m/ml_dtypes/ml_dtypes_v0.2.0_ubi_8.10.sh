82042feb45e12cf72ebca4cc0f38b248aa6d5e02
Stuti Ravikiran Wali <Stuti.Wali@ibm.com>
2026-08-04 16:26:48 +0530

    Update ml_dtypes_v0.2.0_ubi_8.10.sh

--- m/ml_dtypes/ml_dtypes_v0.2.0_ubi_8.10.sh
+++ m/ml_dtypes/ml_dtypes_v0.2.0_ubi_8.10.sh
@@ -4,7 +4,7 @@
 # Package       : ml_dtypes
 # Version       : v0.2.0
 # Source repo   : https://github.com/jax-ml/ml_dtypes.git
-# Tested on     : UBI:9.3
+# Tested on     : UBI:8.10
 # Language      : Python, C
 # Ci-Check  : True
 # Script License: Apache License, Version 2 or later
@@ -104,26 +104,14 @@ export CC=/opt/rh/gcc-toolset-13/root/bin/gcc
 export CXX=/opt/rh/gcc-toolset-13/root/bin/g++
 
 #install
-if ! ( pip install .) ; then
+if ! ( python3.11 -m pip install .) ; then
     echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
     exit 1
 fi
 
-# NOTE:
-# The following two tests are skipped for ml_dtypes v0.5.4 on Python 3.13:
-#   - CustomFloatNumPyTest.testArange_float8_e4m3b11fnuz
-#   - CustomFloatNumPyTest.testArange_float8_e4m3b11fnuz_multi_threaded
-#
-# Reason:
-# These tests use np.testing.assert_equal to compare arrays containing NaN values.
-# NumPy defines NaN != NaN, so the assertion is mathematically invalid.
-# Python 3.13 exposes this deterministically.
-#
-# This is a known upstream test bug fixed after v0.5.4 but not backported.
-# Package functionality is correct; only the test assertion is incorrect.
-if ! pytest -k "not testArange_float8_e4m3b11fnuz"; then
+if ! python3.11 -m pytest; then
     echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
     echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
